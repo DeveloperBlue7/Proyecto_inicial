22d904ae8f6b4a49ffadaf86479e0499246c3172
DeveloperBlue7 <chris-developer@hotmail.com>
2022-07-20 16:22:53 -0500

    Shopping cart

--- Script.js
+++ Script.js
@@ -3,19 +3,30 @@ console.log('hola hay alguien')
 const menu = document.querySelector('.navbar-email');
 const menu2 = document.querySelector('.desktop-menu');
 const mobileiconmenu = document.querySelector('.menu');
-const mobilemenu = document.querySelector('.mobile-menu')
+const mobilemenu = document.querySelector('.mobile-menu');
+const shopping_cart = document.querySelector('.shopping_cart');
+const product_detail = document.querySelector('.product-detail')
+
 
 menu.addEventListener('click', vistatemporal);
-mobileiconmenu.addEventListener("click", vistatemporal2)
+mobileiconmenu.addEventListener("click", vistatemporal2);
+shopping_cart.addEventListener('click', vistatemporal3);
 
 function vistatemporal() {
     menu2.classList.toggle('vista')
+    mobilemenu.classList.add('vista')
 }
 
 function vistatemporal2(){
     mobilemenu.classList.toggle('vista')
+    product_detail.classList.add('vista')
 }
 
+function vistatemporal3(){
+    product_detail.classList.toggle('vista')
+    mobilemenu.classList.add('vista')
+
+}
 
 
   
